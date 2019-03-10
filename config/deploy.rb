d33a1075854d1e90c43f80eb789b31b29e5cb7af
大庭正考 <oobatadashikou@MacBook-Pro.local>
2019-03-10 15:35:56 +0900

    test

---
+++ config/deploy.rb
@@ -0,0 +1,43 @@
+# config valid only for current version of Capistrano
+lock '3.11.0'
+
+set :application, 'mercari_40'
+set :repo_url,  'git@github.com:wish911wish/mercari_40.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_files, %w{ config/secrets.yml }
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1'
+
+set :ssh_options, auth_methods: ['publickey'],
+                 keys: ['/Users/kawakaminozomu/.ssh/mercari_40_key.pem']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+}
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
+
+end
